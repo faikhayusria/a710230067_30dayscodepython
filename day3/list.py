afab7fbbaf86c9400636c97694e9dcade1491fba
faikhayusria <a710230067@student.ums.ac.id>
2024-06-11 15:09:59 +0700

    Create list.py

---
+++ day3/list.py
@@ -0,0 +1,17 @@
+# Definisi list
+angka = [1, 2, 3, 4, 5]
+
+# Menambahkan elemen ke dalam list
+angka.append(6)
+
+# Menghapus elemen dari list
+angka.remove(3)
+
+# Mengakses elemen dari list
+elemen_pertama = angka[0]
+
+# Mengurutkan list
+angka.sort()
+
+print("List setelah penambahan dan penghapusan:", angka)
+print("Elemen pertama dalam list:", elemen_pertama)
