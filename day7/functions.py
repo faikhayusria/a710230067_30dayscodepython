88b8d621a73e4a1a76caa1891641966510def63b
faikhayusria <a710230067@student.ums.ac.id>
2024-06-15 00:22:27 +0700

    Create functions.py

---
+++ day7/functions.py
@@ -0,0 +1,8 @@
+# Definisi fungsi
+def hitung_luas_persegi(sisi):
+    return sisi * sisi
+
+# Menggunakan fungsi
+sisi_persegi = 4
+luas_persegi = hitung_luas_persegi(sisi_persegi)
+print(f"Luas persegi dengan sisi {sisi_persegi} adalah {luas_persegi}")
